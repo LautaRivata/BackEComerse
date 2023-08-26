d73c482127be015c2ca767905de6e5b63c9edcdf
Kementary <Lautarivata@gmail.com>
2023-08-26 15:49:18 -0300

    Incorporo el fetch a FakeStore

--- src/middlewares/getProducts.middleware.js
+++ src/middlewares/getProducts.middleware.js
@@ -2,6 +2,7 @@
 
 import { ProductsModel } from "../models"
 import { sleep } from "../utils"
+const axios = require("axios")
 
 /**
  *
@@ -11,13 +12,23 @@ import { sleep } from "../utils"
  */
 
 const getProductsMiddleware = async (req, res, next) => {
-	await sleep(2000)
-
-	const products = await ProductsModel.readAll()
-
-	req.dataToSend = products
-	req.statusCode = 200
-
+	let data = []
+	let status = 400
+	//con axios a https://fakestoreapi.com/products
+	await axios
+		.get("https://fakestoreapi.com/products")
+		.then(function (response) {
+			data = response.data
+			status = 200
+			console.log(response.data)
+		})
+		.catch(function (error) {
+			console.log(error)
+		})
+		.finally(function () {
+			req.dataToSend = data
+			req.statusCode = status
+		})
 	next()
 }
 
